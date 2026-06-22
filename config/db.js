2165c7eaa37689f23a5270eb64592b020cf356ba
Asif Mamun Hridoy <53093846+hriidzhridoy@users.noreply.github.com>
2026-06-22 14:32:35 +0600

    db file added

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
 
 const connectDB = async () => {
   try {
+    if (dns.getServers().includes("127.0.0.1")) {
+      dns.setServers(["1.1.1.1", "8.8.8.8"]);
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
